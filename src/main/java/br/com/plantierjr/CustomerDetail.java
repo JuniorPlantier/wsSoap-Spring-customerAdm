ff2bb87d2802e8f6fe4b69c4ec1733abe4dc10a6
Carlos Plantier Jr <junior.plantier@gmail.com>
2020-06-15 08:23:01 -0300

    Add serviço de delete

--- src/main/java/br/com/plantierjr/CustomerDetail.java
+++ src/main/java/br/com/plantierjr/CustomerDetail.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2020.06.15 às 07:32:34 AM BRT 
+// Gerado em: 2020.06.15 às 07:50:47 AM BRT 
 //
 
 
--- src/main/java/com/plantier/soap/ws/wsSoapcustomersAdm/soap/CustomerDetailEndpoint.java
+++ src/main/java/com/plantier/soap/ws/wsSoapcustomersAdm/soap/CustomerDetailEndpoint.java
@@ -12,6 +12,8 @@ import com.plantier.soap.ws.wsSoapcustomersAdm.bean.Customer;
 import com.plantier.soap.ws.wsSoapcustomersAdm.service.CustomerDetailService;
 
 import br.com.plantierjr.CustomerDetail;
+import br.com.plantierjr.DeleteCustomerRequest;
+import br.com.plantierjr.DeleteCustomerResponse;
 import br.com.plantierjr.GetAllCustomerDetailRequest;
 import br.com.plantierjr.GetAllCustomerDetailResponse;
 import br.com.plantierjr.GetCustomerDetailRequest;
@@ -40,6 +42,14 @@ public class CustomerDetailEndpoint {
 		return convertToGetAllCustomerDetailResponse(customers);
 	}
 	
+	@PayloadRoot(namespace="http://plantierjr.com.br", localPart="DeleteCustomerRequest")
+	@ResponsePayload
+	public DeleteCustomerResponse deleteCustomerRequest(@RequestPayload DeleteCustomerRequest req) {
+		DeleteCustomerResponse resp = new DeleteCustomerResponse();
+		resp.setStatus(convertToStatusSoap(service.deleteById(req.getId())));
+		return resp;
+	}
+	
 	private GetCustomerDetailResponse convertToGetCustomerDetailResponse(Customer customer) {
 		GetCustomerDetailResponse resp = new GetCustomerDetailResponse();
 		resp.setCustomerDetail(convertToCustomerDetail(customer));
@@ -61,5 +71,12 @@ public class CustomerDetailEndpoint {
 		return cd;
 	}
 	
-	
+	private br.com.plantierjr.Status convertToStatusSoap(com.plantier.soap.ws.wsSoapcustomersAdm.helper.Status statusService) {
+		
+		if(statusService == com.plantier.soap.ws.wsSoapcustomersAdm.helper.Status.FAILURE) {
+			return br.com.plantierjr.Status.FAILURE;
+		} else {
+			return br.com.plantierjr.Status.SUCCESS;
+		}
+	}
 }
